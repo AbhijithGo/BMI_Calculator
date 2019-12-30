811a83331dd50f63f7c4eb9fad78dc15eb2e06e8
AbhijithGo <abhijithgopan19@gmail.com>
2019-12-30 19:09:50 +0530

    set card layout and dark theme

--- lib/main.dart
+++ lib/main.dart
@@ -1,10 +1,17 @@
 import 'package:flutter/material.dart';
+import './input_page.dart';
 
 void main() => runApp(MyApp());
 
 class MyApp extends StatelessWidget {
   @override
   Widget build(BuildContext context) {
-    return Container();
+    return MaterialApp(
+      theme: ThemeData.dark(),
+      title: "BMI Calc",
+      home: InputPage()
+    );
   }
 }
+
+
