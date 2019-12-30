f804fa97f51fae25be55166a6b6e3aeb244a7c61
AbhijithGo <abhijithgopan19@gmail.com>
2019-12-31 05:26:43 +0530

    implementd the result page, seperated constants and files, added navigation.

---
+++ lib/screens/results_page.dart
@@ -0,0 +1,63 @@
+import 'package:bmi_calc/components/reusable_card.dart';
+import 'package:flutter/material.dart';
+import 'package:flutter/widgets.dart';
+import '../constants.dart';
+import '../components/reusable_card.dart';
+import 'package:bmi_calc/components/bottom_button.dart';
+
+class Results extends StatelessWidget {
+  @override
+  Widget build(BuildContext context) {
+    return Scaffold(
+      appBar: AppBar(
+        title: Text('BMI Calculator'),
+      ),
+      body: Column(
+        crossAxisAlignment: CrossAxisAlignment.stretch,
+        mainAxisAlignment: MainAxisAlignment.spaceEvenly,
+        children: <Widget>[
+          Expanded(
+            child: Container(
+              padding: EdgeInsets.all(30),
+              child: Text(
+                'Your Result',
+                style: kTitleTextStyle,
+              ),
+            ),
+          ),
+          Expanded(
+            flex: 5,
+            child: ReusableCard(
+              colour: kActiveCardColor,
+              cardChild: Column(
+                mainAxisAlignment: MainAxisAlignment.spaceEvenly,
+                crossAxisAlignment: CrossAxisAlignment.center,
+                children: <Widget>[
+                  Text(
+                    'Healthy',
+                    style: kResultTextStyle,
+                  ),
+                  Text(
+                    '18.3',
+                    style: kBMITextStyle,
+                  ),
+                  Text(
+                    'Your BMI result is low. Eat more, BITCH',
+                    style: kBodyTextStyle,
+                    textAlign: TextAlign.center,
+                  ),
+                ],
+              ),
+            ),
+          ),
+          BottomButton(
+            buttonTitle: "RECALCULATE",
+            onTap: (){
+              Navigator.pop(context);
+            },
+          ),
+        ],
+      )
+    );
+  }
+}
